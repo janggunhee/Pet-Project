51f4a9760445f767d7a6c3ba0a906b29f36b1b09
huskyhoochu <dfg1499@gmail.com>
2017-11-21 15:08:35 +0900

    settings.py에 django_extensions 설치

--- petweb/config/settings/base.py
+++ petweb/config/settings/base.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third-party app
+    'django_extensions',
     'rest_framework',
     # User app
     'account',
