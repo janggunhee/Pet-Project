92eac4de873b859094658c4bc287f9032e5803ae
huskyhoochu <dfg1499@gmail.com>
2017-11-23 03:36:28 +0900

    urls.py 정규표현식 수정 및 주석 첨가

--- petweb/account/urls.py
+++ petweb/account/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import url
 from . import apis
 
 urlpatterns = [
+    # 유저 로그인
     url(r'^login/$', apis.Login.as_view()),
+    # 유저 회원가입
     url(r'^signup/$', apis.Signup.as_view()),
+    # 유저 디테일 뷰 / 닉네임 수정 / 유저 삭제
     url(r'^detail/(?P<user_pk>\d+)/$', apis.UserDetail.as_view()),
-    url(r'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9a-z]+)/', apis.Activate.as_view(), name='activate'),
+    # 유저 회원가입 후 이메일 활성화 뷰
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9a-z]+)/$', apis.Activate.as_view(), name='activate'),
 ]
