6e15129fec402a6ce9d3780a6475893b35ca7bfc
huskyhoochu <dfg1499@gmail.com>
2017-11-22 11:45:49 +0900

    url 수정

--- petweb/account/urls.py
+++ petweb/account/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^login/$', views.Login.as_view()),
     url(r'^signup/$', views.Signup.as_view()),
-    url(r'^delete/(?P<pk>\d+)/$', views.Delete.as_view()),
+    url(r'^detail/(?P<pk>\d+)/$', views.UserDetail.as_view()),
 ]
