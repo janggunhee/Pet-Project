34759febe37008c843190bb6edda71ec4b3577e8
huskyhoochu <dfg1499@gmail.com>
2017-11-23 16:04:50 +0900

    url resolve name 추가

--- petweb/account/urls.py
+++ petweb/account/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # 유저 로그인
     url(r'^login/$', apis.Login.as_view()),
     # 유저 회원가입
-    url(r'^signup/$', apis.Signup.as_view()),
+    url(r'^signup/$', apis.Signup.as_view(), name='signup'),
     # 유저 디테일 뷰 / 닉네임 수정 / 유저 삭제
     url(r'^detail/(?P<user_pk>\d+)/$', apis.UserDetail.as_view()),
     # 유저 회원가입 후 이메일 활성화 뷰
