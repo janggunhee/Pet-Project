b5f573723f3241b5dff77610232ca28e2694f851
huskyhoochu <dfg1499@gmail.com>
2017-11-23 11:06:39 +0900

    celery 기본설정

--- petweb/config/settings/__init__.py
+++ petweb/config/settings/__init__.py
@@ -1,4 +1,9 @@
 import os
+from ..celery import app as celery_app
+
+# celery.py의 app 변수를 celery_app으로 이름붙이고
+# settings.__init__에 추가한다
+__all__ = ['celery_app']
 
 # 세팅 모듈의 기본값은 로컬을 사용한다
 SETTINGS_MODULE = os.environ.get('DJANGO_SETTINGS_MODULE')
