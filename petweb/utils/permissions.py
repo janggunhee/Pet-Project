bd863b0ae1a6f535f775f96937fe21883e64c6df
huskyhoochu <dfg1499@gmail.com>
2017-11-28 20:52:32 +0900

    custom permissons 수정

--- petweb/utils/permissions.py
+++ petweb/utils/permissions.py
@@ -1,9 +1,9 @@
 from rest_framework import permissions
 
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
+class IsUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == request.user
+        return obj == request.user
 
