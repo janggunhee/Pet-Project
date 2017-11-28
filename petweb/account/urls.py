905c69c582868bc0101cf6b29e6dc7419ac94132
huskyhoochu <dfg1499@gmail.com>
2017-11-28 20:53:03 +0900

    User APIView 코드 리팩토링에 따른 url 수정

--- petweb/account/urls.py
+++ petweb/account/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # 유저 회원가입
     url(r'^signup/$', apis.Signup.as_view(), name='signup'),
     # 유저 디테일 뷰 / 닉네임 수정 / 유저 삭제
-    url(r'^detail/(?P<user_pk>\d+)/$', apis.UserDetail.as_view()),
+    url(r'^detail/(?P<user_pk>\d+)/$', apis.UserDetailUpdateDestroy.as_view()),
     # 유저 회원가입 후 이메일 활성화 뷰
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9a-z]+)/$', apis.Activate.as_view(), name='activate'),
 ]
