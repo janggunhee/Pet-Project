1d364e001013fa2f5f5aa1ef0fda6875af68b1d0
huskyhoochu <dfg1499@gmail.com>
2017-11-30 22:17:11 +0900

    pet model을 위한 permission 추가

--- petweb/utils/permissions.py
+++ petweb/utils/permissions.py
@@ -7,3 +7,9 @@ class IsUserOrReadOnly(permissions.BasePermission):
             return True
         return obj == request.user
 
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
