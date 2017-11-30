fe4838bb56aa90739d98d5d494696ff6c76339d3
huskyhoochu <dfg1499@gmail.com>
2017-11-30 22:29:11 +0900

    serializer pagination 구현

--- petweb/account/apis/api_pet.py
+++ petweb/account/apis/api_pet.py
@@ -4,8 +4,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from utils.permissions import IsOwnerOrReadOnly
+from ..pagination import StandardPetViewPagination
 from ..models import Pet
-from ..serializers import UserSerializer, PetSerializer
+from ..serializers import PetSerializer
 
 __all__ = (
     'PetListCreate',
@@ -13,13 +14,6 @@ __all__ = (
 
 
 """
-generics.ListCreateAPIView
-1. 로그인한 상태에서 내 펫의 목록을 보는 뷰 (get)
-    1. 시리얼라이저 페이지네이션 필요
-    2. 커스텀 필터링
-2. 내 펫을 생성하는 뷰 (post)
-    1. 생성용 시리얼라이저 필요
-
 generics.RetrieveUpdateDestroyAPIView
 3. 하나의 펫을 디테일하게 보는 뷰 (get)
 4. 펫의 내용을 수정하는 뷰 (patch)
@@ -31,7 +25,8 @@ generics.RetrieveUpdateDestroyAPIView
 
 class PetListCreate(generics.ListCreateAPIView):
     serializer_class = PetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+    pagination_class = StandardPetViewPagination
+    permission_classes = (IsOwnerOrReadOnly, )
     lookup_url_kwarg = 'user_pk'
 
     def get_queryset(self):
