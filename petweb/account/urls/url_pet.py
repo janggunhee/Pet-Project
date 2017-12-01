54fc8edda2e2efcdd829c58cd274b44935e75222
huskyhoochu <dfg1499@gmail.com>
2017-12-01 11:35:27 +0900

    url reverse name 변경

--- petweb/account/urls/url_pet.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from .. import apis
-
-urlpatterns = [
-    url('^pets/', apis.PetListCreate.as_view(), name='pet-list')
-]
--- petweb/account/urls/url_profile.py
+++ petweb/account/urls/url_profile.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url, include
 
-from . import url_pet
 from .. import apis
 
 urlpatterns = [
     # 유저 디테일 뷰 / 닉네임 수정 / 유저 삭제
     url(r'^(?P<user_pk>\d+)/$', apis.UserProfileUpdateDestroy.as_view(), name='user'),
-    url(r'^(?P<user_pk>\d+)/', include(url_pet, namespace='pet')),
+]
+
+urlpatterns += [
+    # 펫 리스트 / 펫 생성
+    url(r'^(?P<user_pk>\d+)/pets/', apis.PetListCreate.as_view(), name='pet'),
 ]
