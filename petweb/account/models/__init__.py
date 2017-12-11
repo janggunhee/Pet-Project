dcaf0d09a18c4aaeab9daa7926eb1b6c482c168a
Kim JinSeok <jskim2code@gmail.com>
2017-12-11 17:56:29 +0900

    versatileimagefield Install::  Pillow를 지우고(pip unintall Pillow) versatileimagefield를 설치(pip install versatileimagefield) settings = INSTALLED_APPS + VERSATILEFEILD_SETTINGS model:  models.thumnail_base.py에서 ThumbnailBaseModel 추상 모델

--- petweb/account/models/__init__.py
+++ petweb/account/models/__init__.py
@@ -1,3 +1,3 @@
 from .model_human import *
 from .model_pet import *
-
+from .thumnail_base import *
---
+++ petweb/account/models/thumnail_base.py
@@ -0,0 +1,24 @@
+from django.db import models
+from versatileimagefield.fields import VersatileImageField, PPOIField
+
+
+class ImageBaseModel(models.Model):
+    class Meta:
+        abstract = True
+    image = VersatileImageField(
+        'Image',
+        upload_to='images/testimagemodel/',
+        width_field='width',
+        height_field='height'
+    )
+    ppoi = PPOIField()
+    height = models.PositiveIntegerField(
+        'Image Height',
+        blank=True,
+        null=True
+    )
+    width = models.PositiveIntegerField(
+        'Image Width',
+        blank=True,
+        null=True
+    )
