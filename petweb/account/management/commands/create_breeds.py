f09b8254fc927465527e8bb03648c3b78be9a560
huskyhoochu <dfg1499@gmail.com>
2017-12-16 21:19:01 +0900

    자동화 명령 추가

---
+++ petweb/account/management/commands/create_breeds.py
@@ -0,0 +1,48 @@
+from django.core.management import BaseCommand
+
+from ...models import PetBreed, PetSpecies
+from utils import pet_age
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not PetBreed.objects.all():
+            for breed in pet_age.Large_dog:
+                PetBreed.objects.create(
+                    species=PetSpecies.objects.get(pet_type='dog'),
+                    breeds_name=breed
+                )
+            for breed in pet_age.Middle_dog:
+                PetBreed.objects.create(
+                    species=PetSpecies.objects.get(pet_type='dog'),
+                    breeds_name=breed
+                )
+            for breed in pet_age.Small_dog:
+                PetBreed.objects.create(
+                    species=PetSpecies.objects.get(pet_type='dog'),
+                    breeds_name=breed
+                )
+            total_dog_length = len(PetBreed.objects.filter(species__pet_type='dog'))
+
+            for breed in pet_age.Large_cat:
+                PetBreed.objects.create(
+                    species=PetSpecies.objects.get(pet_type='cat'),
+                    breeds_name=breed
+                )
+            for breed in pet_age.Middle_cat:
+                PetBreed.objects.create(
+                    species=PetSpecies.objects.get(pet_type='cat'),
+                    breeds_name=breed
+                )
+            for breed in pet_age.Small_cat:
+                PetBreed.objects.create(
+                    species=PetSpecies.objects.get(pet_type='cat'),
+                    breeds_name=breed
+                )
+            total_cat_length = len(PetBreed.objects.filter(species__pet_type='cat'))
+
+            print(f'total created dogs : {total_dog_length}')
+            print(f'total created cats : {total_cat_length}')
+
+        else:
+            print('pet breeds are already exists.')
