afbe7a5510d58ecda76e29a232397f6b280efb05
huskyhoochu <dfg1499@gmail.com>
2017-12-16 22:52:25 +0900

    path return none 삭제

--- petweb/config/storages.py
+++ petweb/config/storages.py
@@ -9,8 +9,3 @@ class StaticStorage(S3Boto3Storage):
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
 
-    # AWS S3는 절대경로를 지원하지 않는다. 따라서 이미지 url을 꺼내려고 할 경우 'NotImplementedError'가 발생한다
-    # 해결책은 이 사이트를 참고했다: http://blog.hardlycode.com/solving-django-storage-notimplementederror-2011-01/
-    # 공식 문서: https://docs.djangoproject.com/en/1.11/ref/files/storage/#django.core.files.storage.Storage.path
-    def path(self, name):
-        return None
