c8dec5cc75bacf217a3f63566c3eadbaed24fe33
huskyhoochu <dfg1499@gmail.com>
2017-12-20 16:25:14 +0900

    url 주석 추가

--- petweb/medical/urls.py
+++ petweb/medical/urls.py
@@ -4,11 +4,13 @@ from . import apis
 
 # 의료정보 관련
 urlpatterns = [
-    url(r'(?P<user_pk>\d+)/pets/(?P<pet_pk>\d+)/$', apis.PetVaccineInoculation.as_view(), name='vaccine'),
+    # 동물이 맞은 백신 정보 리스트 / 생성
+    url(r'(?P<user_pk>\d+)/pets/(?P<pet_pk>\d+)/vaccines/$', apis.PetVaccineInoculation.as_view(), name='vaccine'),
 
 ]
 
 # 지도 관련
 urlpatterns += [
-    url(r'search-hospital/', apis.Hospital.as_view(), name='hospital'),
+    # 주변 동물병원 검색
+    url(r'search-hospital/$', apis.Hospital.as_view(), name='hospital'),
 ]
