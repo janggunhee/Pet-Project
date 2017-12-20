d421817e52384b472eaca521e740819bc3a4e364
huskyhoochu <dfg1499@gmail.com>
2017-12-20 16:25:50 +0900

    백신 접종 정보에 대한 뷰와 시리얼라이저 추가

--- petweb/medical/apis.py
+++ petweb/medical/apis.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from utils import near_by_search
+from utils.rest_framework import permissions
 from .models import PetMedical
-from .serializers import HospitalSerializer
+from .serializers import HospitalSerializer, VaccineInoculationSerializer
 
 
 # 주변 병원을 검색해주는 뷰
@@ -37,11 +38,12 @@ class Hospital(APIView):
 
 
 # 펫이 맞은 백신을 검색해주는 뷰
-class PetVaccineInoculation(generics.RetrieveAPIView):
+class PetVaccineInoculation(generics.ListCreateAPIView):
+    serializer_class = VaccineInoculationSerializer
+    permission_classes = (permissions.IsMedicalOwnerOrReadOnly,)
 
     def get_queryset(self):
         user = self.kwargs['user_pk']
         pet = self.kwargs['pet_pk']
         instance = PetMedical.objects.filter(pet__owner_id=user).get(pet_id=pet)
         return instance.inoculation_set.all()
-
--- petweb/medical/serializers.py
+++ petweb/medical/serializers.py
@@ -1,6 +1,43 @@
 from rest_framework import serializers
 
+from .models import VaccineInoculation, Vaccine
 
+
+# 주변 병원 검색 시리얼라이저
 class HospitalSerializer(serializers.Serializer):
     lat = serializers.CharField()
     lng = serializers.CharField()
+
+
+# 백신의 정보를 보여주는 시리얼라이저
+class VaccineInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vaccine
+        fields = (
+            'species',
+            'name',
+            'turn',
+            'period',
+        )
+
+
+# 동물이 맞은 백신 정보를 보여주는 시리얼라이저
+class VaccineInoculationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VaccineInoculation
+        fields = (
+            'medical',
+            'vaccine',
+            'num_of_times',
+            'inoculated_date',
+            'hospital',
+            'is_alarm',
+        )
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        data = {
+            'vaccine_info': VaccineInfoSerializer(instance.vaccine).data,
+            'inoculation_info': ret
+        }
+        return data
