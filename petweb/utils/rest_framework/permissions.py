f7d1f961c830e3bf5702ea5c1f6d453c1c81f4e5
huskyhoochu <dfg1499@gmail.com>
2017-12-20 16:24:38 +0900

    medical을 위한 permission 새로 생성

--- petweb/utils/rest_framework/permissions.py
+++ petweb/utils/rest_framework/permissions.py
@@ -18,3 +18,10 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+
+class IsMedicalOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.pet.owner == request.user
