44b13505696a3f080400530f7af3bcacbc574083
huskyhoochu <dfg1499@gmail.com>
2017-12-21 23:57:37 +0900

    PetAge 코드 리팩토링, 쓸모 없는 시리얼라이저 삭제

--- petweb/account/apis/api_pet.py
+++ petweb/account/apis/api_pet.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 
 from django.contrib.auth import get_user_model
-from django.shortcuts import get_object_or_404
-from rest_framework import generics, status, permissions
+from rest_framework import generics, status
 from rest_framework.response import Response
 
-from utils.rest_framework import pagination, permissions as custom_permissions
+from utils.rest_framework import pagination, permissions
 from utils.functions import pet_age
 from ..models import Pet, PetSpecies, PetBreed
 from ..serializers import *
@@ -23,7 +22,7 @@ __all__ = (
 # 펫 리스트 / 생성 뷰
 class PetListCreate(generics.ListCreateAPIView):
     serializer_class = PetCreateSerializer
-    permission_classes = (custom_permissions.IsUserOrReadOnly,)
+    permission_classes = (permissions.IsUserOrReadOnly,)
     pagination_class = pagination.StandardPetViewPagination
 
     def get_queryset(self):
@@ -40,7 +39,7 @@ class PetListCreate(generics.ListCreateAPIView):
 # 펫 디테일 보기 뷰 / 정보 수정 / 펫 삭제
 class PetProfile(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PetCreateSerializer
-    permission_classes = (custom_permissions.IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsOwnerOrReadOnly,)
     lookup_url_kwarg = 'pet_pk'
 
     def get_queryset(self):
@@ -50,39 +49,17 @@ class PetProfile(generics.RetrieveUpdateDestroyAPIView):
 
 # 펫 사람 나이 환산 뷰
 class PetAge(generics.GenericAPIView):
-    queryset = Pet.objects.all()
-    serializer_class = PetSerializer
-    permission_classes = (custom_permissions.IsOwnerOrReadOnly,)
-    lookup_field = ('owner_id', 'pk')
-    lookup_url_kwarg = ('user_pk', 'pet_pk')
+    lookup_url_kwarg = 'pet_pk'
 
-    # 쿼리셋에서 객체를 가져오는 메소드
-    def get_object(self):
-        # 커스텀 세팅: 반려동물 쿼리셋을 가져올 때 필터링 옵션을 준다
-        # 동물 주인의 pk값과 url에 들어온 user_pk 값이 일치하는 동물들만 가져오도록!
-        # 쿼리셋: 위에서 정의한 펫 쿼리셋
-        queryset = self.filter_queryset(self.get_queryset())
-
-        filter_kwargs = {
-            key: self.kwargs[value]
-            # lookup_field와 lookup_url_kwarg를 동시에 순회하면서
-            # key에는 lookup_field의 값을,
-            # value에는 전체 키워드 인자 묶음 중 lookup_url_kwarg를 key로 하는 값을 넣는다
-            for key, value in zip(self.lookup_field, self.lookup_url_kwarg)
-        }
-
-        # 쿼리셋과 키워드 인자 묶음으로 펫 인스턴스를 받는다
-        obj = get_object_or_404(queryset, **filter_kwargs)
-        # 인스턴스의 권한을 체크한다
-        self.check_object_permissions(self.request, obj)
-
-        return obj
+    def get_queryset(self):
+        user = self.kwargs['user_pk']
+        return Pet.objects.filter(owner_id=user)
 
     def get(self, request, *args, **kwargs):
         # 반려동물의 생년월일을 datetime 객체로 리턴하는 함수
         def pet_datetime_birth_date(serializer):
             # 입력값에서 birth_date를 가져온다
-            raw_birth_date = serializer.data['birth_date']
+            raw_birth_date = serializer.data['pet']['birth_date']
             # 문자열 값인 raw_birth_date를 datetime 객체로 바꾼다
             datetime_birth_date = datetime.strptime(raw_birth_date, '%Y-%m-%d').date()
             return datetime_birth_date
@@ -95,8 +72,8 @@ class PetAge(generics.GenericAPIView):
         # 반려동물이 사람으로 치면 몇 살인지를 계산하는 함수
         def human_age_conversion(serializer):
             # 입력값에서 species와 breed 값을 가져와 각 모델에서 객체를 꺼낸다
-            object_pet_type = PetSpecies.objects.get(pet_type=serializer.data['species'])
-            object_pet_breed = PetBreed.objects.get(breeds_name=serializer.data['breeds'])
+            object_pet_type = PetSpecies.objects.get(pet_type=serializer.data['pet']['species'])
+            object_pet_breed = PetBreed.objects.get(breeds_name=serializer.data['pet']['breeds'])
             # 각 객체의 이름을 문자열로 꺼낸다
             str_pet_type = object_pet_type.pet_type
             str_pet_breed = object_pet_breed.breeds_name
@@ -109,7 +86,7 @@ class PetAge(generics.GenericAPIView):
         # 이상 없으면 펫 객체 디테일을 생성
         # PetSerializer가 HyperlinkedidentifyField를 갖게 되어서
         # 시리얼라이저에 request를 전달해준다
-        serializer = PetSerializer(instance, context={'request': request})
+        serializer = PetCreateSerializer(instance, context={'request': request})
         # 펫의 생년월일
         pet_birth_date = pet_datetime_birth_date(serializer)
         # 펫의 나이에서 개월 수 제외하고 년도만 출력
--- petweb/account/serializers/serializer_pet.py
+++ petweb/account/serializers/serializer_pet.py
@@ -16,9 +16,7 @@ User = get_user_model()
 __all__ = (
     'PetSpeciesField',
     'PetBreedField',
-    'PetSerializer',
     'PetCreateSerializer',
-    'PetEditSerializer',
     'PetBreedSerializer',
 )
 
@@ -90,44 +88,6 @@ class PetBreedField(serializers.RelatedField):
 
 
 # 펫의 내용을 보여주는 시리얼라이저
-class PetSerializer(serializers.ModelSerializer):
-    # 펫 종류는 PetSpeciesSerializer로 가공된다
-    species = PetSpeciesField()
-    # 펫 품종은 PetBreedSerializer로 가공된다
-    breeds = PetBreedField()
-
-    # 펫 나이는 PetAge 뷰를 URL 값으로 보여주도록 설계
-    # 여러 개의 키워드 인자 값을 받기 위해 필드를 커스텀
-
-    ages = MultiplePKsHyperlinkedIdentityField(
-        view_name='profile:pet-age',
-        lookup_fields=['owner_id', 'pk'],
-        lookup_url_kwargs=['user_pk', 'pet_pk']
-    )
-
-    class Meta:
-        model = Pet
-        fields = (
-            'pk',  # 동물pk
-            'species',  # 강아지/고양이
-            'breeds',  # 품종
-            'name',  # 이름
-            'birth_date',  # 생년월일
-            'gender',  # 성별
-            'body_color',  # 색깔
-            'identified_number',  # 동물등록번호
-            'is_neutering',  # 중성화
-            'is_active',  # 활성화여부(동물사망/양도/입양)
-            'ages',  # 나이
-            'image',
-        )
-        read_only_fields = (
-            'pk',
-            'ages',
-        )
-
-
-# 펫을 생성할 때 사용하는 시리얼라이저
 class PetCreateSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=100)  # 이름
     # http://www.django-rest-framework.org/api-guide/fields/#datefield
@@ -189,109 +149,6 @@ class PetCreateSerializer(serializers.ModelSerializer):
         }
 
         return data
-
-
-# 펫 정보를 수정하는 시리얼라이저
-class PetEditSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=100)  # 이름
-    # http://www.django-rest-framework.org/api-guide/fields/#datefield
-    birth_date = serializers.DateField(format=api_settings.DATE_FORMAT)  # 생년월일
-    # RelatedField의 to_interval_value를 이용해 입력값을 제어
-    # 참고: https://goo.gl/roNmog
-    species = PetSpeciesField()  # 강아지/고양이
-    breeds = PetBreedField()  # 품종
-    # http://www.django-rest-framework.org/api-guide/fields/#choicefield
-    gender = serializers.ChoiceField(choices=Pet.CHOICE_GENDER)  # 성별
-    body_color = serializers.ChoiceField(choices=Pet.CHOICE_COLOR)  # 색상
-    identified_number = serializers.CharField(max_length=20)  # 동물등록번호
-    is_neutering = serializers.BooleanField()  # 중성화
-    is_active = serializers.BooleanField()  # 활성화
-    ages = MultiplePKsHyperlinkedIdentityField(
-        view_name='profile:pet-age',
-        lookup_fields=['owner_id', 'pk'],
-        lookup_url_kwargs=['user_pk', 'pet_pk']
-    )  # 나이
-
-    class Meta:
-        model = Pet
-        fields = (
-            'pk',  # 동물pk
-            'species',  # 강아지/고양이
-            'breeds',  # 품종
-            'name',  # 이름
-            'birth_date',  # 생년월일
-            'gender',  # 성별
-            'body_color',  # 색깔
-            'identified_number',  # 동물등록번호
-            'is_neutering',  # 중성화
-            'is_active',  # 활성화여부(동물사망/양도/입양)
-            'ages',  # 나이
-            'image',
-        )
-        read_only_fields = (
-            'pk',
-            'ages',
-        )
-
-    # 출력 형식을 커스터마이징
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        data = {
-            'owner': UserSerializer(instance.owner).data,
-            'pet': ret
-        }
-
-        return data
-
-    # 입력 형식을 커스터마이징
-    def to_internal_value(self, data):
-        # 입력한 데이터(dict 형식)를 key와 value로 나누어 순회한다
-        result = {}
-        for key, value in data.items():
-            # 만일 value가 없다면, 즉 사용자가 변경 사항을 아무것도 입력하지 않았다면
-            if not value:
-                # 원래 객체가 가지고 있던 값을 입력한다
-                result[key] = getattr(self.instance, f'{key}')
-            else:
-                # 만일 변경 사항이 있다면 그 값을 입력한다
-                result[key] = value
-
-        return result
-
-    # 업데이트 메소드 커스터마이징
-    def update(self, instance, validated_data):
-        # 원래 update 메소드를 긁어와서 커스텀
-        raise_errors_on_nested_writes('update', self, validated_data)
-        info = model_meta.get_field_info(instance)
-
-        # validated_data를 키와 값으로 나누어 순회한다
-        for attr, value in validated_data.items():
-            # 만일 attr 값이 species고 value의 type이 문자열이라면
-            # 즉, 사용자가 값을 바꾸려고 한다면
-            if attr == 'species' and type(value) == str:
-                # 사용자가 바꾸려고 하는 값의 펫 종류 객체를 가져온다
-                pet_species_instance = PetSpecies.objects.get(pet_type=value)
-                # 펫 인스턴스의 species 값을 새로운 펫 종류 객체로 교체한다
-                # https://docs.python.org/3/library/functions.html#setattr
-                setattr(instance, attr, pet_species_instance)
-            # 만일 attr 값이 breeds고 value의 type이 문자열이라면
-            elif attr == 'breeds' and type(value) == str:
-                # 사용자가 바꾸려고 하는 값의 펫 품종 객체를 가져온다
-                pet_breed_instance = PetBreed.objects.get(breeds_name=value)
-                # 펫 인스턴스의 breeds 값을 새로운 펫 품종 객체로 교체한다
-                setattr(instance, attr, pet_breed_instance)
-            # 나머지는 update 원래 메소드 가져옴
-            elif attr in info.relations and info.relations[attr].to_many:
-                # https://docs.python.org/3/library/functions.html#getattr
-                field = getattr(instance, attr)
-                field.set(value)
-            else:
-                setattr(instance, attr, value)
-        instance.save()
-
-        thumbnail_pet = making_thumbnail(instance)
-
-        return thumbnail_pet
 
 
 # 사용자가 강아지/고양이를 선택하면 펫 품종을 보여주는 시리얼라이저
