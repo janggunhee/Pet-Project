181af88e4dd46db0abd9c8177ba2d575bfa93a7d
huskyhoochu <dfg1499@gmail.com>
2017-12-21 22:26:47 +0900

    PetListCreate 코드 리팩토링

--- petweb/account/apis/api_pet.py
+++ petweb/account/apis/api_pet.py
@@ -21,102 +21,20 @@ __all__ = (
 
 
 # 펫 리스트 / 생성 뷰
-class PetListCreate(generics.GenericAPIView):
-    """
-    펫의 리스트를 보고 펫을 생성하는 뷰
-
-    1. 리스트 보기
-        method: get
-
-    2. 펫 생성
-        method: post
-
-    """
-    # # 쿼리셋: 반려동물 쿼리셋 전체
-    # queryset = Pet.objects.all()
-    # # 시리얼라이저: 펫 시리얼라이저
-    # serializer_class = PetSerializer
-    # 페이지네이션: utils.pagination에 있는 pagination 사용
+class PetListCreate(generics.ListCreateAPIView):
+    serializer_class = PetCreateSerializer
+    permission_classes = (custom_permissions.IsUserOrReadOnly,)
     pagination_class = pagination.StandardPetViewPagination
-    # 권한: 소유주 이외에는 읽기만 가능
-    permission_classes = (custom_permissions.IsOwnerOrReadOnly,)
-    # url 키워드 인자: user_pk
-    lookup_url_kwarg = 'user_pk'
-
-    # 데이터를 시리얼라이징해서 생성하는 메소드
-    def perform_create(self, serializer):
-        # AbstractBaseUser가 is_active=False 로 설정되어 있어서
-        # Pet도 이 설정을 그대로 상속받는 이슈가 있었다
-        # PetCreateSerializer에서 default=True로 설정하는 것으로 변경
-        serializer.save(owner=self.request.user)
-
-    # 쿼리셋에서 객체를 가져오는 메소드
-    def get_object(self):
-        # 커스텀 세팅: 반려동물 쿼리셋을 가져올 때 필터링 옵션을 준다
-        # 동물 주인의 pk값과 url에 들어온 user_pk 값이 일치하는 동물들만 가져오도록!
-        filter_kwargs = {'id': self.kwargs[self.lookup_url_kwarg]}
-        # 필터링을 거친 쿼리셋을 리스트로 반환한다
-        obj = get_object_or_404(self.get_queryset(), **filter_kwargs)
-
-        # 리스트의 권한을 체크한다
-        self.check_object_permissions(self.request, obj)
-
-        return obj
 
     def get_queryset(self):
-        if self.request.method in permissions.SAFE_METHODS:
-            return User.objects.all()
-        return Pet.objects.all()
+        user = self.kwargs['user_pk']
+        return Pet.objects.filter(owner_id=user)
 
-    def get_serializer_class(self):
-        if self.request.method in permissions.SAFE_METHODS:
-            return UserSerializer
-        return PetCreateSerializer
-
-    # 펫 리스트를 가져오는 뷰
-    # method: get
-    def get(self, request, *args, **kwargs):
-        # 앞서 get_object 메소드로 가져온 instance 객체를 불러온다
-        instance = self.get_object()
-
-        # # instance 객체를 페이지네이션 된 쿼리셋으로 변환한다
-        # page = self.paginate_queryset(instance)
-        # # 쿼리셋의 숫자가 많아서 page가 생성된다면
-        # if page is not None:
-        #     serializer = self.get_serializer(page, many=True)
-        #     # 페이지네이션 응답을 리턴한다
-        #     return self.get_paginated_response(serializer.data)
-
-        # 만일 쿼리셋의 숫자가 적어서 page가 만들어지지 않는다면
-        user_serializer = self.get_serializer(instance)
-        data = {
-            'owner': user_serializer.data,
-            'pets': PetSerializer(instance.pets, many=True, context={'request': request}).data
-        }
-
-        # 일반 시리얼라이저 데이터를 리턴한다
-        return Response(data, status=status.HTTP_200_OK)
-
-    # 펫을 생성하는 뷰
-    # method: post
-    def post(self, request, *args, **kwargs):
-        # 만일 post 요청을 보낸 user pk 값과 url에 담긴 user_pk 키워드 인자 값이 일치한다면
-        # (자기 자신이 시도해야만 펫 생성이 가능하도록 설계한 것)
-        if str(request.user.pk) == request.resolver_match.kwargs['user_pk']:
-            # 위의 조건문을 만족하면 펫을 생성한다
-            # mixins.CreateModelMixin의 create 함수를 그대로 가져옴
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        # 만일 어떤 유저가 다른 유저의 user_pk값으로 펫을 생성하려고 시도한다면
-        # 조건문을 만족하지 못해 에러 메시지를 출력한다
-        error = {
-            "detail": "You do not have permission to perform this action."
-        }
-
-        return Response(error, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        user = self.kwargs['user_pk']
+        instance = User.objects.get(pk=user)
+        self.check_object_permissions(self.request, instance)
+        serializer.save(owner=instance)
 
 
 # 펫 사람 나이 환산 뷰
